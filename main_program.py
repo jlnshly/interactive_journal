72cbcda2bc0d3da778131e6d37faa56538a21e80
jlnshly <jelaineashleyramos@gmail.com>
2026-04-27 00:53:14 +0800

    code for main program

--- main_program.py
+++ main_program.py
@@ -3,9 +3,27 @@ import time
 
 def start_interactive_journal():
     journal = InteractiveJournal()
-    print("What's on your mind?")
+    print('What is on your mind?')
     active = True
-    user_input = input("Enter line: ")
-    journal.add_entry = user_input
-    choice = input("Would you like to add more lines (y/n): ").lower().strip()
+    while active:
+        user_input = input('Enter line: ')
+        journal.add_entry = user_input
+        choice = input('Would you like to add more lines (y/n): y or n').lower().strip()
+        if choice == 'n':
+            active = False
+        elif choice != 'y':
+            print('Invalid input. Exiting...')
+            exit()
+    print('Let me finalize your entries...')
+    time.sleep(1)
+    if journal.save_entry():
+        print('Entry saved.')
+    else:
+        print('Entry not saved.')
+
+if __name__ == '__main__':
+    start_interactive_journal()
+
+
+
 
