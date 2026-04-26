1f470b060438627c80d618c5af97dc5c030611fe
jlnshly <jelaineashleyramos@gmail.com>
2026-04-27 00:45:46 +0800

    adding display messages for user

--- main_program.py
+++ main_program.py
@@ -5,5 +5,7 @@ def start_interactive_journal():
     journal = InteractiveJournal()
     print("What's on your mind?")
     active = True
-    user_input = input()
+    user_input = input("Enter line: ")
+    journal.add_entry = user_input
+    choice = input("Would you like to add more lines (y/n): ").lower().strip()
 
