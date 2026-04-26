2285a219b939a307cc1b70172acace13806de422
jlnshly <jelaineashleyramos@gmail.com>
2026-04-27 00:42:43 +0800

    importing functions

---
+++ main_program.py
@@ -0,0 +1,9 @@
+from journal_functions import InteractiveJournal
+import time
+
+def start_interactive_journal():
+    journal = InteractiveJournal()
+    print("What's on your mind?")
+    active = True
+    
+
