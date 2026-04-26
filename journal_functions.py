8788d48a824e70a812be9167b394a8e2e737583c
jlnshly <jelaineashleyramos@gmail.com>
2026-04-27 01:02:49 +0800

    indentation changed since code wasnt working

--- journal_functions.py
+++ journal_functions.py
@@ -4,21 +4,21 @@ class InteractiveJournal:
         self.filename = filename
         self.entries = []
 
-        def add_entry(self, text: str):
-            """Adds a new line to temporary session list"""
-            if text.strip():
-                self.entries.append(text)
+    def add_entry(self, note):
+        """Adds a new line to temporary session list"""
+        if note.strip():
+            self.entries.append(note)
 
-        def save_entry(self):
-            """Saves all session entries to file"""
-            if not self.entries:
-                return False
-            try:
-                with open(self.filename, "a") as file:
-                    for line in self.entries:
-                        file.write(line + "\n")
-                return True
-            except Exception as e:
-                print(f"Error saving your entry: {e}")
-                return False
+    def save_entry(self):
+        """Saves all session entries to file"""
+        if not self.entries:
+            return False
+        try:
+            with open(self.filename, "a") as file:
+                for line in self.entries:
+                    file.write(line + "\n")
+            return True
+        except Exception as e:
+            print(f"Error saving your entry: {e}")
+            return False
 
